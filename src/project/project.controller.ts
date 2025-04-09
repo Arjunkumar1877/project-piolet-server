f0e9b6fba677c8f632894d9396fd7cfbca79633c
Arjunkumar1877 <arjun.tech177@gmail.com>
2025-04-10 01:08:15 +0530

    feat: add members and projects

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ProjectsService } from './project.service';
+import { CreateProjectDto, CreateTeamMemberDto } from './project.dto';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post()
-  create(@Body() createProjectDto: any) {
+  create(@Body() createProjectDto: CreateProjectDto) {
     return this.projectsService.create(createProjectDto);
   }
+  
+  @Post('team-members')
+  createTeamMembers(@Body() createTeamMembers: CreateTeamMemberDto){
+    return this.projectsService.createTeamMembers(createTeamMembers)
+  }
+
 
   @Get()
   findAll() {
---
+++ src/schemas/team-member.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type TeamMemberDocument = TeamMember & Document;
+
+@Schema({ timestamps: true })
+export class TeamMember {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  role: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Project' }] })
+  projects: Types.ObjectId[];
+}
+
+export const TeamMemberSchema = SchemaFactory.createForClass(TeamMember); 
