1020ef826b47d7e17960d29b6408d9bdb7d4d874
Arjunkumar1877 <arjun.tech177@gmail.com>
2025-04-16 01:55:50 +0530

    fix: update create task api

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,10 +3,11 @@ import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from 'src/schemas/task-schema';
+import { Project, ProjectSchema } from 'src/schemas/project-schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }, { name: Project.name, schema: ProjectSchema }]),
   ],
   providers: [TaskService],
   controllers: [TaskController]
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -3,14 +3,25 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Task, TaskDocument } from 'src/schemas/task-schema';
 import { CreateTaskDto, UpdateTaskDto } from './task.dto';
+import { Project, ProjectDocument } from 'src/schemas/project-schema';
 @Injectable()
 export class TaskService {
-    constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>){}
+    constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>,
+    @InjectModel(Project.name) private projectModel: Model<ProjectDocument>){}
 
+    
     async create(createTaskDto: CreateTaskDto): Promise<Task> {
-        const createdTask = new this.taskModel(createTaskDto);
-        return createdTask.save();
-    }
+        const createdTask = await this.taskModel.create(createTaskDto);
+      
+        await this.projectModel.findByIdAndUpdate(
+          createTaskDto.project,
+          { $push: { tasks: createdTask._id } },
+          { new: true }
+        );
+      
+        return createdTask;
+      }
+      
     async findAll(id: string): Promise<Task[]> {
         const tasks = await this.taskModel.find({ project: id }).populate('project').exec();
         return tasks;
